950a7c51e482f7a25d759041e6c211796dcbe1f9
= <=>
2024-04-28 16:15:48 +0200

    modif

--- festival_influenceur/js/jquery.countdown.js
+++ festival_influenceur/js/jquery.countdown.js
@@ -137,8 +137,8 @@
 			@property [isRTL=false] {boolean} True for right-to-left languages, false for left-to-right. */
 		regionalOptions: { // Available regional settings, indexed by language/country code
 			'': { // Default regional settings - English/US
-				labels: ['Years', 'Months', 'Weeks', 'Days', 'Hours', 'Minutes', 'Seconds'],
-				labels1: ['Year', 'Month', 'Week', 'Day', 'Hour', 'Minute', 'Second'],
+				labels: ['Years', 'Months', 'Weeks', 'Jours', 'Heures', 'Minutes', 'Secondes'],
+				labels1: ['Annee', 'Moi', 'Week', 'Jour', 'Heure', 'Minute', 'Seconde'],
 				compactLabels: ['y', 'm', 'w', 'd'],
 				whichLabels: null,
 				digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
